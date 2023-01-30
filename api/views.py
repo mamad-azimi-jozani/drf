8729b35b9d9c4fdd43de4db9de7a3432aa5743e6
mohmmad <azimi.jozani.mohammad@gmail.com>
2023-01-30 17:30:06 +0330

    create product model and api_view

--- api/views.py
+++ api/views.py
@@ -1,4 +1,11 @@
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.models import Product
+
+
+@api_view()
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hello world by django"})
+
+    return Response({'message': 'hello world'})
